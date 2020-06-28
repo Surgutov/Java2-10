dc647c267db3d5aa2b0d297b6c6896d96b131cb1
surgutov <surgutovpersonal@gmail.com>
2020-06-28 11:19:05 +0300

    new version

--- src/main/java/ru/netology/stats/StatsService.java
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -5,10 +5,9 @@ public class StatsService {
     public long sum(long[] purchases) {
         long sum = 0;
         for (long purchase : purchases) {
-            sum = purchase + sum;
+            sum += purchase;
         }
         return sum;
-
     }
 
     public long average(long[] purchases) {
@@ -16,5 +15,78 @@ public class StatsService {
         return sum / purchases.length;
     }
 
+    public long monthMaxSales(long[] purchases) {
+        long max = findMax(purchases);
+        int monthNumb = 0;
+        int monthNumbMax = 0;
+        for (long purchase : purchases) {
+            monthNumb += 1;
+            if (purchase == max) {
+                monthNumbMax = monthNumb;
+
+            }
+        }
+        return monthNumbMax;
+    }
+
+    public long monthMinSales(long[] purchases) {
+        long min = findMin(purchases);
+        int monthNumb = 0;
+        int monthNumbMin = 0;
+        for (long purchase : purchases) {
+            monthNumb += 1;
+            if (purchase == min) {
+                monthNumbMin = monthNumb;
+
+            }
+        }
+        return monthNumbMin;
+    }
+
+    public long lowAverage(long[] purchases) {
+        long average = average(purchases);
+        int count = 0;
+        for (long purchase : purchases) {
+            if (purchase < average) {
+                count += 1;
+            }
+        }
+        return  count;
+    }
+
+    public long upperAverage(long[] purchases) {
+        long average = average(purchases);
+        int count = 0;
+        for (long purchase : purchases) {
+            if (purchase > average) {
+                count += 1;
+            }
+        }
+        return  count;
+    }
+
+
+
+
+
+    public long findMax(long[] purchases) {
+        long max = purchases[0];
+        for (long purchase : purchases) {
+            if (purchase > max) {
+                max = purchase;
+            }
+        }
+        return max;
+    }
+
+    public long findMin(long[] purchases) {
+        long min = purchases[0];
+        for (long purchase : purchases) {
+            if (purchase < min) {
+                min = purchase;
+            }
+        }
+        return min;
+    }
 
 }
--- src/test/java/ru/netology/stats/StatsServiceTest.java
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -8,20 +8,46 @@ class StatsServiceTest {
     StatsService service = new StatsService();
     long[] purchases = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
 
-    //В таком виде должно быть реализовано? Если да, то на выходных допишу.
-
     @Test
     void shouldCalculateSumOfSalesInYear() {
         long expected = 180;
         long actual = service.sum(purchases);
         assertEquals(expected, actual);
+
     }
 
     @Test
-    void shouldCalculateAveragefSales() {
+    void shouldCalculateAverageSum() {
         long expected = 15;
         long actual = service.average(purchases);
         assertEquals(expected, actual);
     }
 
+    @Test
+    void numberMonthMaxSales() {
+        long expected = 8;
+        long actual = service.monthMaxSales(purchases);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void numberMonthMinSales() {
+        long expected = 9;
+        long actual = service.monthMinSales(purchases);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void monthCountLowAverage() {
+        long expected = 5;
+        long actual = service.lowAverage(purchases);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void monthCountUpperAverage() {
+        long expected = 5;
+        long actual = service.upperAverage(purchases);
+        assertEquals(expected, actual);
+    }
 }
